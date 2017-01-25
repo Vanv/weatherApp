e0e2bec4732072a4b35e0e7685d7462dd1fcbfd2
Van Vairavan <van@MacUsers-MacBook-Pro.local>
2017-01-26 08:37:47 +1300

    updated the script to get the required datas from openweather API

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-//Ajax call to get Latitude and Longitude
-$.ajax({
-    url: "https://ipapi.co/json",
+//Call to get Latitude and Longitude
+
+var latitude, longitude;
+
+$.getJSON('http://ip-api.com/json', {}).done(function(location) {
+
+	latitude = location.lat;
+	longitude = location.lon;
+	
+	onGettingCoords();
+})
+
+function onGettingCoords() {
+    console.log("test " + latitude, longitude);
+    $.ajax({
+    url : "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=e04539b40b013f15e67b723be6769496",
     dataType: "json", 
     success: function ( data, status, XHR ) {
-        latlong(data);
+        weather(data);
+        console.log(data.name, data.id);
     }
 });
+} 
 
-function latlong(responseData ) {
-
-    var lat = responseData.latitude;
-    var long = responseData.longitude;
-    console.log("Latitude & longitude " + lat + ", "+long);
-
-    // return lat, long;
+function weather(data) {
+	console.log(data.main.humidity);
+	$(".temp").text(data.main.temp);
+	$(".clouds").text(data.weather[0].description);
+	$(".city").text(data.name);
+	$(".country").text(data.sys.country);
+	
+	$(".humid").text(data.main.humidity);
+	$(".wind").text(data.wind.speed);
+	$(".sunrise").text(data.sys.sunrise);
+	$(".sunset").text(data.sys.sunset);
+	
 }
