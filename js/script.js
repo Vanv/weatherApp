edfa229fe7aaaacdb6004a3f6a9c36013abef69a
Van Vairavan <van@MacUsers-MacBook-Pro.local>
2017-01-26 23:44:43 +1300

    implemented the toggle function for Celsius to Fahrenheit and sunset, sunrise timings

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ $.getJSON('http://ip-api.com/json', {}).done(function(location) {
 	onGettingCoords();
 })
 
+//Passing Latitude and Longitude
 function onGettingCoords() {
     console.log("test " + latitude, longitude);
     $.ajax({
@@ -17,13 +18,11 @@ function onGettingCoords() {
     dataType: "json", 
     success: function ( data, status, XHR ) {
         weather(data);
-        console.log(data.name, data.id);
     }
 });
 } 
-
+//Weather Data
 function weather(data) {
-	console.log(data.main.humidity);
 	$(".temp").text(data.main.temp);
 	$(".clouds").text(data.weather[0].description);
 	$(".city").text(data.name);
@@ -31,7 +30,31 @@ function weather(data) {
 	
 	$(".humid").text(data.main.humidity);
 	$(".wind").text(data.wind.speed);
-	$(".sunrise").text(data.sys.sunrise);
-	$(".sunset").text(data.sys.sunset);
 	
+	sunriseValue = data.sys.sunrise;
+	sunriseTime1 = new Date(sunriseValue*1000);
+	sunriseTime2 = sunriseTime1.getHours() + ":" + sunriseTime1.getMinutes();
+	// var today = get Hourstime();
+	$(".sunrise").text(sunriseTime2);
+	
+	$(".sunset").text(data.sys.sunset);
+	sunsetValue = data.sys.sunset
+	sunsetTime1 = new Date(sunsetValue*1000);
+	sunsetTime2 = sunsetTime1.getHours() + ":" + sunsetTime1.getMinutes();
+	$(".sunset").text(sunsetTime2);
+	
+	//Unit Conversion - Celsius to Fahrenheit
+	var temp = Math.round(JSON.stringify(data.main.temp));
+     //cel to Ferenheit
+     var fh = temp * 9 / 5 + 32;
+     //fh to cel1
+     var cel = (fh - 32) * 5 / 9;
+	  $('.cel').on('click', function() {
+      	$('.temp').html(cel + " <sup>0</sup>C");
+     });
+
+     $('.fh').on('click', function() {
+      	$('.temp').html(fh + " F");
+     });
 }
+
